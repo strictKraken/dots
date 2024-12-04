5c328a544ea73a95a90461ab1595ebbee8ea46a2
Stephan <stephan@pop-os.lan>
2024-12-04 20:49:09 +0300

    feat: add nvim

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,26 @@
+return {
+  -- github themes
+  -- github_dark_defult
+  -- github_ligth
+  -- colorscheme theme_name
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+
+      vim.cmd("colorscheme github_dark")
+    end,
+  },
+  -- set deafault theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark_default",
+    },
+  },
+}
